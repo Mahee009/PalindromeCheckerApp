7f9d2b1fd4b11610deed5d50ca6a41bd2ad7d326
Mahee009 <maheetibrewal@gmail.com>
2026-03-11 14:51:01 +0530

    UC4 completed

--- App/src/Palindrome.java
+++ App/src/Palindrome.java
@@ -2,26 +2,33 @@ public class Palindrome {
 
     public static void main(String[] args) {
 
+
         java.util.Scanner scanner = new java.util.Scanner(System.in);
 
         // Take input from user
         System.out.print("Enter text: ");
         String input = scanner.nextLine();
 
-        // Variable to store reversed string
-        String reversed = "";
+        // Convert string to character array
+        char[] chars = input.toCharArray();
 
-        // Reverse the string using for loop
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed = reversed + input.charAt(i);
+        // Two-pointer approach
+        int start = 0;
+        int end = chars.length - 1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
 
-        // Compare original and reversed string
-        if (input.equals(reversed)) {
-            System.out.println("Input text: " + input + " is it palindrome? : true");
-        } else {
-            System.out.println("Input text: " + input + " is it palindrome? : false");
-        }
+        // Display result
+        System.out.println("Input text: " + input + " is it palindrome? : " + isPalindrome);
 
         scanner.close();
     }
