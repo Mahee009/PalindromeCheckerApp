d99f8fb057c68eb68c8276e8f6d1c816d3e47456
Mahee009 <maheetibrewal@gmail.com>
2026-03-11 14:31:07 +0530

    UC2 completed

--- App/src/Palindrome.java
+++ App/src/Palindrome.java
@@ -1,8 +1,22 @@
 public class Palindrome {
     static void main() {
-        System.out.println("welcome to palidrome checker app");
-        System.out.println("version 1.0");
-        System.out.println("syetem initialized successfully");
 
+        // Hardcoded string
+        String text = "madam";
+
+        // Reverse the string
+        String reversed = "";
+
+        for (int i = text.length() - 1; i >= 0; i--) {
+            reversed = reversed + text.charAt(i);
+        }
+
+        // Check palindrome condition
+        if (text.equals(reversed)) {
+            System.out.println("Input text: " + text + " is it palindrome? : true");
+        } else {
+            System.out.println("Input text: " + text + " is it palindrome? : false");
+        }
     }
 }
+
