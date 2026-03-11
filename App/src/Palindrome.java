0496a8e000e08009c99b6406da5c0d1275f9ae45
Mahee009 <maheetibrewal@gmail.com>
2026-03-11 14:06:27 +0530

    [mahee]added uc1

---
+++ App/src/Palindrome.java
@@ -0,0 +1,8 @@
+public class Palindrome {
+    static void main() {
+        System.out.println("welcome to palidrome checker app");
+        System.out.println("version 1.0");
+        System.out.println("syetem initialized successfully");
+
+    }
+}
